a8b9cca94d7f6d1d05f7975f766e4fb75f91ed70
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-22 19:57:28 +0300

    first

---
+++ InternetCafe/src/internetcafe/DashboardController.java
@@ -0,0 +1,18 @@
+
+package internetcafe;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class DashboardController implements Initializable {
+    
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
