97b8af8a58cf6fb2ab5808ad6c346422e1d33cc4
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-23 14:17:40 +0300

    three tabpanes

--- InternetCafe/src/internetcafe/DashboardController.java
+++ InternetCafe/src/internetcafe/DashboardController.java
@@ -1,12 +1,24 @@
 
 package internetcafe;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.input.MouseEvent;
 
 
 public class DashboardController implements Initializable {
+
+    @FXML
+    private FontAwesomeIconView addComp;
+    @FXML
+    private TabPane mainTab;
+    @FXML
+    private Button btnsession;
     
 
     
@@ -14,5 +26,15 @@ public class DashboardController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void addCompPage(MouseEvent event) {
+        mainTab.getSelectionModel().select(2);
+    }
+
+    @FXML
+    private void startSession(MouseEvent event) {
+        
+    }
     
 }
